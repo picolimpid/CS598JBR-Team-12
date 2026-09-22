85f8e8e201d91a853144e4275f48febde358fd31
Andy <hanqim2@illinois.edu>
2026-09-22 02:23:25 +0000

    test push

--- MP1/commands.py
+++ MP1/commands.py
@@ -3,7 +3,7 @@
 ###################################################################
 
 # TODO: Update Your NetIDs in alphabetical order
-NetIDs = ["sampleID1", "sampleID2", "sampleID3", "sampleID4"]
+NetIDs = ["hanqim2", ,"pochunk2","yhl7"]
 NetIDs_str = " ".join(NetIDs)
 
 # TODO: Clone your GitHub repository
